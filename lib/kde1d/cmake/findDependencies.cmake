dc89e37723e488a4aec3d9ef896ee7fcd72cb671
Thibault Vatter <thibault.vatter@gmail.com>
2022-01-06 17:17:22 -0500

    test

--- lib/kde1d/cmake/findDependencies.cmake
+++
@@ -1,5 +0,0 @@
-include(cmake/findEigen3.cmake            REQUIRED)
-message(STATUS "Eigen3 version: ${EIGEN3_VERSION}")
-find_package(Boost 1.56                   REQUIRED)
-
-set(external_includes ${EIGEN3_INCLUDE_DIR} ${Boost_INCLUDE_DIRS})
--- lib/kde1d/include/kde1d.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include "kde1d/kde1d.hpp"
