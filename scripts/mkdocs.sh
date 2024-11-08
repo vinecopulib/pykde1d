c75e15879ae95cccbd452cec1814583b829d3038
Thibault Vatter <thibault.vatter@gmail.com>
2024-11-08 12:04:52 +0100

    Update docs

--- scripts/mkdocs.sh
+++ scripts/mkdocs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Extracts the doxygen documentation from the kde1d sources
 
-echo "Using libclang at ${LIBCLANG_PATH}"
+
 echo "Using boost at ${Boost_INCLUDE_DIR}"
 echo "Using eigen3 at ${EIGEN3_INCLUDE_DIR}"
-python3 -m pybind11_mkdoc -I 'lib/kde1d/include' -I $Boost_INCLUDE_DIR  -I ${EIGEN3_INCLUDE_DIR} $(find lib/kde1d/include -regextype awk -regex ".*.hpp" -print) --output 'src/docstr.hpp'
+python3 scripts/mkdocs.py -I'lib/vinecopulib/include' -I'/usr/include' $(find lib/kde1d/include -regextype awk -regex ".*.hpp" -print) -output='src/include/docstr.hpp'
